e00c8cca7a007bd9052e79a7f3b40e649ee40e08
Dmitrij Velström <dmitrij.vel@live.se>
2020-10-26 13:04:58 +0100

    solved rot13 kata

---
+++ 5kuy/Rot13/main.ts
@@ -0,0 +1,22 @@
+/*
+
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
+ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13.
+If there are numbers or special characters included in the string, they should be returned as they are.
+Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+*/
+
+console.log(rot13("test")); // grfg
+
+function rot13(message: string) {
+  return message.split("").map((letter) => getCorrespondingChar(letter));
+
+  const alphabet: string = "abcdefghijklmnopqrstuvwxyz";
+  function getCorrespondingChar(letter: string) {
+    const letterIndex: number = alphabet.indexOf(letter);
+    const targetIndex: number = letterIndex + 13 <= 25 ? letterIndex + 13 : 25 - letterIndex;
+  }
+}
